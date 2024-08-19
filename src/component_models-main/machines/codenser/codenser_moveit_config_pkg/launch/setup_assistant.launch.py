e12d9c7e5422b54798cfbf20c131a3e82cd2d723
Vaidya-Dhruval <dhruvalde.v29@gmail.com>
2024-08-19 13:03:57 +0200

    cleaning

--- src/component_models-main/machines/codenser/codenser_moveit_config_pkg/launch/setup_assistant.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_setup_assistant_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("m_0330939_001", package_name="m_0330939_001_moveit_config_pkg").to_moveit_configs()
-    return generate_setup_assistant_launch(moveit_config)
--- src/component_models-main/std_components/m_2025642_001/m_2025642_001_moveit_config_pkg/launch/warehouse_db.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_warehouse_db_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("m_2025642_001", package_name="m_2025642_001_moveit_config_pkg").to_moveit_configs()
-    return generate_warehouse_db_launch(moveit_config)
